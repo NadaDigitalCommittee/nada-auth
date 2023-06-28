c5fd7d7c8ad579069f72a466a41ddfefe9011596
Googlefan256 <private@googlefan.net>
2023-06-29 02:11:21 +0900

    feat: 🎸 auth

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,16 @@ const app = express();
 import { config } from "dotenv";
 config();
 import axios from "axios";
+import {
+  APIGuildMember,
+  ButtonStyle,
+  MessageFlags,
+  Routes,
+} from "discord-api-types/v10";
+import { ActionRowBuilder, ButtonBuilder } from "@discordjs/builders";
+import { REST } from "@discordjs/rest";
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN as string);
 
 interface ModalData {
   custom_id: string;
@@ -26,7 +36,7 @@ app.post(
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
           data: {
             content: "あなたにはこのコマンドの実行権限がありません",
-            flags: 1 << 6,
+            flags: MessageFlags.Ephemeral,
           },
         });
       }
@@ -34,34 +44,111 @@ app.post(
         content:
           "サーバーでの禁止行為\n・過度な連投行為（10秒の間に5回以上送信する）をしたとき、10分間メッセージを送れません。（一時ミュート）\n・三回の警告を受けるとメッセージを送れません。（ミュート）\n\n・ミュートは管理者による注意のあと解除されます。\n\nこのサーバーは以下の人が利用します。\n・デジタル委員\n・Adobeライセンス利用希望者\n・Adobeライセンス利用者\n・デジタル委員会を利用する生徒会員\n・放送委員\n・顧問及びその他教職員\n\nこのサーバーの使い方\n1、灘生であることを確認するため下のボタンからフォームに回答して下さい(確認が取れ次第、管理者がロール付与します。)\n2、ロール選択 の説明を読んで必要なものを選択してください。\n3、その他のロールが必要なときは role-request に、書いてある説明通りに送ってください。",
         components: [
-          {
-            type: 1,
-            components: [
-              { custom_id: "answer", label: "回答する", style: 1, type: 2 },
-            ],
-          },
+          new ActionRowBuilder<ButtonBuilder>()
+            .addComponents(
+              new ButtonBuilder()
+                .setCustomId("answer")
+                .setLabel("回答する")
+                .setStyle(ButtonStyle.Primary)
+            )
+            .toJSON(),
         ],
       });
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
           content: "送信しました",
-          flags: 1 << 6,
+          flags: MessageFlags.Ephemeral,
         },
       });
     } else if (interaction.type === InteractionType.MESSAGE_COMPONENT) {
+      if (
+        interaction.data.custom_id === "yes" ||
+        interaction.data.custom_id === "no"
+      ) {
+        const isOk = interaction.data.custom_id === "yes";
+        const id = interaction.message.embeds[0]?.footer?.text;
+        if (!id)
+          return res.send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+              content: "メッセージを正しく解釈できませんでした。",
+              flags: MessageFlags.Ephemeral,
+            },
+          });
+        return rest
+          .get(Routes.guildMember(interaction.guild_id, id))
+          .then((rmember) => {
+            const member = rmember as APIGuildMember;
+            if (member.roles.includes(process.env.VERIFY_ROLE ?? "")) {
+              return res.send({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                  content:
+                    "このユーザーは既に確認が取れているため回答する必要はありません。",
+                  flags: MessageFlags.Ephemeral,
+                },
+              });
+            }
+            if (isOk) {
+              return rest
+                .put(
+                  Routes.guildMemberRole(
+                    interaction.guild_id,
+                    id,
+                    process.env.VERIFY_ROLE ?? ""
+                  )
+                )
+                .then(() => {
+                  return res.send({
+                    type: InteractionResponseType.UPDATE_MESSAGE,
+                    data: {
+                      content: "ロールを付与しました。",
+                      components: [],
+                    },
+                  });
+                })
+                .catch((_e) => {
+                  return res.send({
+                    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                    data: {
+                      content: "ロールの付与中にエラーが発生しました。",
+                      flags: MessageFlags.Ephemeral,
+                    },
+                  });
+                });
+            } else {
+              return res.send({
+                type: InteractionResponseType.UPDATE_MESSAGE,
+                data: {
+                  content: "ロールを付与しませんでした。",
+                  components: [],
+                },
+              });
+            }
+          })
+          .catch((_e) => {
+            return res.send({
+              type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+              data: {
+                content: "ロールの取得中にエラーが発生しました。",
+                flags: MessageFlags.Ephemeral,
+              },
+            });
+          });
+      }
       if (interaction.member.roles.includes(process.env.VERIFY_ROLE)) {
         return res.send({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
           data: {
             content:
               "あなたは既に確認が取れているため回答する必要はありません。",
-            flags: 1 << 6,
+            flags: MessageFlags.Ephemeral,
           },
         });
       }
       return res.send({
-        type: InteractionResponseType.APPLICATION_MODAL,
+        type: InteractionResponseType.MODAL,
         data: {
           components: [
             {
@@ -71,7 +158,7 @@ app.post(
               min_length: 2,
               required: true,
               style: 1,
-              type: 4
+              type: 4,
             },
             {
               label: "組",
@@ -80,7 +167,7 @@ app.post(
               min_length: 1,
               required: true,
               style: 1,
-              type: 4
+              type: 4,
             },
             {
               label: "番号",
@@ -89,7 +176,7 @@ app.post(
               min_length: 1,
               required: true,
               style: 1,
-              type: 4
+              type: 4,
             },
             {
               label: "名前",
@@ -98,14 +185,14 @@ app.post(
               min_length: 2,
               required: true,
               style: 1,
-              type: 4
+              type: 4,
             },
           ].map((x) => ({ type: 1, components: [x] })),
           title: "回答して下さい",
           custom_id: "modal",
         },
       });
-    } else if (interaction.type === InteractionType.APPLICATION_MODAL_SUBMIT) {
+    } else if (interaction.type === InteractionType.MODAL_SUBMIT) {
       const values = interaction.data.components.map(
         (x: { components: Array<ModalData> }) => x.components[0].value
       );
@@ -126,13 +213,32 @@ app.post(
                 },
               ],
               author: {
-                name: `${interaction.member.user.username}#${interaction.member.user.discriminator}(${interaction.member.user.id})`,
+                name: `${interaction.member.user.username}(${interaction.member.user.id})`,
                 icon_url: interaction.member.user.avatar
                   ? `https://cdn.discordapp.com/avatars/${interaction.member.user.id}/${interaction.member.user.avatar}.png`
                   : undefined,
               },
               color: 3447003,
+              footer: {
+                text: `${interaction.member.user.id}`,
+              },
             },
+          ],
+          components: [
+            new ActionRowBuilder<ButtonBuilder>()
+              .addComponents(
+                new ButtonBuilder()
+                  .setCustomId("yes")
+                  .setLabel("承認")
+                  .setStyle(ButtonStyle.Success)
+              )
+              .addComponents(
+                new ButtonBuilder()
+                  .setCustomId("no")
+                  .setLabel("否認")
+                  .setStyle(ButtonStyle.Danger)
+              )
+              .toJSON(),
           ],
         })
         .then(() => {
@@ -141,7 +247,7 @@ app.post(
             data: {
               content:
                 "回答を送信しました。\nロール付与までしばらくお待ちください。",
-              flags: 1 << 6,
+              flags: MessageFlags.Ephemeral,
             },
           });
         })
@@ -151,7 +257,7 @@ app.post(
             data: {
               content:
                 "回答を送信できませんでした。\nしばらく時間をおいてもう一度お試しください。",
-              flags: 1 << 6,
+              flags: MessageFlags.Ephemeral,
             },
           });
         });
--- src/webhook.ts
+++ src/webhook.ts
@@ -5,7 +5,19 @@ import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v10";
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN as string);
 rest
-  .post(Routes.channelWebhooks(process.env.CHANNEL_ID as string), {
-    body: { name: "ニトリ" },
+  .post(Routes.channelWebhooks(process.env.RULE_CHANNEL_ID as string), {
+    body: { name: "ルール" },
   })
-  .then(console.log);
+  .then((x) => {
+    console.log("Rule Webhook");
+    console.log(x);
+  });
+
+rest
+  .post(Routes.channelWebhooks(process.env.ANSWER_CHANNEL_ID as string), {
+    body: { name: "回答" },
+  })
+  .then((x) => {
+    console.log("Answer Webhook");
+    console.log(x);
+  });
